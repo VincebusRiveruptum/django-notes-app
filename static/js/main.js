fe9d4c77dba1a83b91706a8e83cef0acf0ae9582
VincebusRiveruptum <vincebus@Vincebus-PC.>
2025-08-14 22:21:01 -0400

    ADD: Note index WIP

--- static/js/main.js
+++ static/js/main.js
@@ -1,4 +1,14 @@
 import "vite/modulepreload-polyfill";
 import "../css/main.css";
 
+import { createIcons, Menu, ArrowRight, Globe } from "lucide";
+
+createIcons({
+  icons: {
+    Menu,
+    ArrowRight,
+    Globe,
+  },
+});
+
 console.log("sup");
